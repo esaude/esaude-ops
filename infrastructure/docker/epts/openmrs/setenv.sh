67ba3045ab81ef9989d0a991d09f9e5b8d53b78e
Nathan Floor <nathan.floor@jembi.org>
2019-04-26 17:15:10 +0200

    Added helpful docker scripts

---
+++ infrastructure/docker/epts/openmrs/setenv.sh
@@ -0,0 +1,1 @@
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Djava.awt.headless=true -XX:+UseConcMarkSweepGC -Xms512m -Xmx2048m -XX:PermSize=256m -XX:MaxPermSize=768m -Duser.timezone=Africa/Maputo"
